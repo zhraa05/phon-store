b1b42ab990ee7ce7b4e92510075a7055a7577b06
zhraa sabah <102597538+zhraa05@users.noreply.github.com>
2022-05-20 09:18:19 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+  import logo from './logo.svg';
+  import './App.css';
+  import 'bootstrap/dist/css/bootstrap.min.css'
+import {Route, Routes } from "react-router-dom"
+import Navbar from './component/Navbar';
+import Prodectlist from './component/Prodectlist';
+import Details from './component/Details';
+import Cart from './component/cart/Cart';
+
+import Diflt from './component/Diflt';
+export default class App extends Component {
+  render() {
+    return (
+
+      
+      <React.Fragment>
+        
+      <Navbar/>
+  
+      <Routes >
+        <Route exact path='/'  element= {<Prodectlist />} />
+        <Route path='/dital' element= {<Details />} />
+        <Route path='/cart' element= {<Cart />} />
+        <Route  element= {<Diflt />} />
+      </Routes>
+
+
+      
+    
+      </React.Fragment>    )
+  }
+}
